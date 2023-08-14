08f6868ed74b2f7c52a238706f86a3bbd44f4d1a
Rik Crompton <rik.crompton@howells-associates.com>
2023-08-14 15:50:32 +0100

    Added a asecond yardarm reference

--- SdkConsumer/Class1.cs
+++ SdkConsumer/Class1.cs
@@ -5,6 +5,7 @@
         public Class1()
         {
             MyApi.Sdk.Api.ICountryEndpoints endpoints = null;
+            MyApi.Sdk.Models.CountryListItem listItem = new MyApi.Sdk.Models.CountryListItem();
         }
     }
 }
